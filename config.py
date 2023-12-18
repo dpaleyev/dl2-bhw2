ba6c08797728bb02b7e8fb3fc9cfd97af7dafb50
Daniel Paleyev <danny.paleyev@gmail.com>
2023-12-18 04:43:08 +0300

    config

---
+++ config.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+
+@dataclass
+class DCGANConfig:
+    dataroot = "/notebooks/images"
+    workers = 6
+    image_size = 64
+    batch_size = 64
+
+    num_epochs = 50
+    learning_rate = 0.0002
+    beta1 = 0.5
+
+    latent_dim = 100
+    hidden_generator = 64
+    hidden_discriminator = 64
+
+DCGAN_config = DCGANConfig()
